1a8f92959b59bf4c88bc6590e1da34183347eeba
Luccas Traumer <lucas.jeesus@hotmail.com>
2022-02-23 20:07:55 -0300

    Create ponteiros.c

---
+++ ponteiros.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main() {
+    //int valor = 'a';
+
+   // float test = 12;
+
+    //printf("Valor e: %f", test);
+
+    int valorPonteiro = 10;
+    int segundoValor = 20;
+
+    int *ptr_Ponteir = &valorPonteiro;
+    int *ptr_SegundoValor = &segundoValor;
+
+    int test;
+    scanf("%i", &test);
+
+    printf("%i \n", ptr_Ponteir);
+    printf("%i \n", ptr_SegundoValor);
+    printf("%i", &test);
+}
